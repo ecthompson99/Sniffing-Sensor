ee05836e5f20c60c1f27cfafbe4dea739bf34ae0
ecthompson99 <ecthompson99@gmail.com>
2019-11-05 18:38:49 -0500

    Added plotting functionality to GUI, and experimenting with multithreading

--- GUI/mplwidget.py
+++ GUI/mplwidget.py
@@ -4,6 +4,7 @@
 from PyQt5.QtWidgets import*
 
 from matplotlib.backends.backend_qt5agg import FigureCanvas
+from matplotlib.backends.backend_qt5agg import (NavigationToolbar2QT as NavigationToolbar)
 
 from matplotlib.figure import Figure
 
@@ -14,10 +15,13 @@ class MplWidget(QWidget):
 
         QWidget.__init__(self, parent)
         
-        self.canvas = FigureCanvas(Figure())
+        self.canvas = FigureCanvas(Figure(figsize=(12,12)))
+        self.canvas.setParent(self)
+        self.toolbar = NavigationToolbar(self.canvas, self)
         
         vertical_layout = QVBoxLayout()
         vertical_layout.addWidget(self.canvas)
+        vertical_layout.addWidget(self.toolbar)
         
         self.canvas.axes = self.canvas.figure.add_subplot(111)
         self.setLayout(vertical_layout)
